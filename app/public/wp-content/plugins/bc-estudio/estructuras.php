6b1989e75ece5fd8b794b1258e5127e4127d317d
Importmatica <biblicomentarios@gmail.com>
2021-07-11 23:07:13 -0500

    Shortcode bc_estructura_volumen_division

--- app/public/wp-content/plugins/bc-estudio/estructuras.php
+++ app/public/wp-content/plugins/bc-estudio/estructuras.php
@@ -41,34 +41,35 @@ order by term_order;";
 
     ob_start();
     ?>
-    <div class="border">
+    <h2>Libros por división</h2>
+    La siguiente relación te mostrará los libros que integran cada división.
+    <div class="row col-12">
         <?php
         foreach ($volumenes as $volumen) {
             ?>
-            <h3 class="text-white p-1 m-0 bg-primary">
-                <a target="_blank" href="/categoria/escrituras/<?= $volumen->slug ?>">
+            <div class="col-12  p-1 m-0 mt-2 mb-1" style="background-color:green;">
+                <a target="_blank" class="text-white" style="font-weight:bold" href="/categoria/escrituras/<?= $volumen->slug ?>">
                     <?= $volumen->name ?>
                 </a>
-            </h3>
-            <div>
+            </div>
             <?php
-                $query = "select wt.term_id,name,slug,description
+            $query = "select wt.term_id,name,slug,description
                     from wp_terms wt join wp_term_taxonomy wtt on wt.term_id = wtt.term_id 
                     where parent=$volumen->term_id
                     and taxonomy='category'
                     order by term_order;";
-                $divisiones = $wpdb->get_results($query);
+            $divisiones = $wpdb->get_results($query);
 
-                foreach ($divisiones as $division){
+            foreach ($divisiones as $division) {
                 ?>
-                      <div class="p-0 pl-2">
-                          <a target="_blank" href="/categoria/escrituras/<?= $volumen->slug ?>/<?= $division->slug ?>">
-                              <?= $division->name ?>
-                          </a>
-                      </div>
-            </div>
-            <?php
-                }
+                <div class="p-0 pl-2 col-12">
+                    <i class="fas fa-book" style="font-size:.8em;color:brown;"></i>
+                    <a target="_blank" href="/categoria/escrituras/<?= $volumen->slug ?>/<?= $division->slug ?>">
+                        <?= $division->name ?>
+                    </a>
+                </div>
+                <?php
+            }
         }
         ?>
     </div>
