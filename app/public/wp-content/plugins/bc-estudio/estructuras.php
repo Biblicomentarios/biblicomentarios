71c110757a95b6e32f385c7c450a9c3d0995aeb1
Importmatica <biblicomentarios@gmail.com>
2021-07-11 22:12:56 -0500

    Se agrega shortcode bc_estructura_volumen_division

--- app/public/wp-content/plugins/bc-estudio/estructuras.php
+++ app/public/wp-content/plugins/bc-estudio/estructuras.php
@@ -11,7 +11,7 @@ where parent=$parent
 and taxonomy='category'
 order by term_order;";
 
-    $volumenes =  $wpdb->get_results($query);
+    $volumenes = $wpdb->get_results($query);
 
     ob_start();
 
@@ -20,7 +20,60 @@ order by term_order;";
         echo '<li> <a target="_blank" href="/categoria/escrituras/' . $volumen->slug . '">' . $volumen->name . '</a>';
     }
     echo '</ol>';
-    
+
     return ob_get_clean();
 }
+
 add_shortcode('bc_estructura_volumen', 'bc_estructura_volumen');
+
+function bc_estructura_volumen_division($attr)
+{
+    global $wpdb;
+    $parent = $attr['parent'];
+
+    $query = "select wt.term_id,name,slug,description
+from wp_terms wt join wp_term_taxonomy wtt on wt.term_id = wtt.term_id 
+where parent=$parent
+and taxonomy='category'
+order by term_order;";
+
+    $volumenes = $wpdb->get_results($query);
+
+    ob_start();
+    ?>
+    <div class="border">
+        <?php
+        foreach ($volumenes as $volumen) {
+            ?>
+            <h3 class="text-white p-1 m-0 bg-primary">
+                <a target="_blank" href="/categoria/escrituras/<?= $volumen->slug ?>">
+                    <?= $volumen->name ?>
+                </a>
+            </h3>
+            <div>
+            <?php
+                $query = "select wt.term_id,name,slug,description
+                    from wp_terms wt join wp_term_taxonomy wtt on wt.term_id = wtt.term_id 
+                    where parent=$volumen->term_id
+                    and taxonomy='category'
+                    order by term_order;";
+                $divisiones = $wpdb->get_results($query);
+
+                foreach ($divisiones as $division){
+                ?>
+                      <div class="p-0 pl-2">
+                          <a target="_blank" href="/categoria/escrituras/<?= $volumen->slug ?>/<?= $division->slug ?>">
+                              <?= $division->name ?>
+                          </a>
+                      </div>
+            </div>
+            <?php
+                }
+        }
+        ?>
+    </div>
+    <?php
+    return ob_get_clean();
+}
+
+add_shortcode('bc_estructura_volumen_division', 'bc_estructura_volumen_division');
